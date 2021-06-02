a5b4c7d39458e7f80bbc9f91dd9a9b0db50b18b6
Minea, Iuliana <iuliana.minea@centric.eu>
2021-06-02 09:21:02 +0300

    update api to netcore v3.1

---
+++ Booking.Api/Booking.Api/Booking.Api/Extensions/FlightExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Booking.Api.Models;
+
+namespace Booking.Api.Extensions
+{
+    public static class FlightExtensions
+    {
+        public static void Update(this FlightModel dbFlight, UpsertFlightModel flight)
+        {
+            dbFlight.From = flight.From;
+            dbFlight.To = flight.To;
+            dbFlight.Price = flight.Price;
+            dbFlight.Date = flight.Date;
+        }
+
+        public static FlightModel Save(this UpsertFlightModel flight)
+        {
+            var newItem = new FlightModel
+                          {
+                              Id = Repository.Flights.Max(f => f.Id) + 1,
+                              To = flight.To,
+                              From = flight.From,
+                              Price = flight.Price,
+                              Date = flight.Date
+                          };
+
+            Repository.Flights.Add(newItem);
+            return newItem;
+        }
+    }
+}
